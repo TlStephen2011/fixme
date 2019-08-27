8211e26f8abcb2c9da8b8b707718a9fab88258d0
tstephen <tstephen@iqbusiness.net>
2019-08-27 19:39:43 +0200

    can send messages

--- fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/App.java
+++ fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/App.java
@@ -1,13 +1,20 @@
 package za.co.wethinkcode;
 
+import java.io.IOException;
+
+import za.co.wethinkcode.broker.Broker;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        Broker b = new Broker();
+        
+       // Thread.sleep(1000);
+        
+        b.sendMessage("exit");
     }
 }
--- fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/broker/Broker.java
+++ fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/broker/Broker.java
@@ -1,12 +1,36 @@
 package za.co.wethinkcode.broker;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Broker {
 
     private final int PORT = 5000;
     private final String HOST = "127.0.0.1";
+    private Socket socket = new Socket(HOST, PORT);
+    
+    public Broker() throws UnknownHostException, IOException{
+        sendMessage("exit");        
+        InputStream input = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-    public Broker() {
+        String time = reader.readLine();
 
+        System.out.println("Server said: " + time);
+        socket.close();
+    }
+
+    public void sendMessage(String msg) throws IOException {
+        OutputStream os = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(os, true);
+
+        writer.println(msg);
     }
 
 }
