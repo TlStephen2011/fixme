1cef6b7a8d3bd6b8156fd1294be3785cce5d34cb
tstephen <tstephen@iqbusiness.net>
2019-08-27 15:48:57 +0200

    working on Broker class

---
+++ fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/broker/Broker.java
@@ -0,0 +1,12 @@
+package za.co.wethinkcode.broker;
+
+public class Broker {
+
+    private final int PORT = 5000;
+    private final String HOST = "127.0.0.1";
+
+    public Broker() {
+
+    }
+
+}
