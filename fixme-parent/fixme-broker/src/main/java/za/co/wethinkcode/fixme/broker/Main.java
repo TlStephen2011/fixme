4e17d98a20ac8d2e78c313d3b645efc5a7e6a495
Monde M <mmbatha@student.wethinkcode.co.za>
2019-08-28 15:36:54 +0200

    Set the main classes

--- fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/fixme/broker/Main.java
+++ fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/fixme/broker/Main.java
@@ -9,7 +9,7 @@ public class Main
 {
     public static void main( String[] args )
     {
-        Client client = new Client("Market");
+        Client client = new Client("Broker");
         Thread clientThread = new Thread(client);
         clientThread.start();
         try {
