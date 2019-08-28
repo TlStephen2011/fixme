6b66acb63f968457f403847c422c108f12b891e3
tstephen <tstephen@iqbusiness.net>
2019-08-28 21:53:39 +0200

    Capitalization server is not responding to client request but is receiving it

--- fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/App.java
+++ fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/App.java
@@ -1,20 +1,27 @@
 package za.co.wethinkcode;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
-import za.co.wethinkcode.broker.Broker;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args ) throws IOException, InterruptedException {
-        Broker b = new Broker();
-        
-       // Thread.sleep(1000);
-        
-        b.sendMessage("exit");
+    public static void main( String[] args ) throws Exception {
+    	try (Socket s = new Socket("127.0.0.1", 5000)) {
+    		System.out.println("Enter lines to capitalize: Ctrl+D to quit");
+    		
+    		Scanner scanner = new Scanner(System.in);
+    		Scanner in = new Scanner(s.getInputStream());
+    		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+    		
+    		while (scanner.hasNextLine()) {
+    			out.println(scanner.nextLine());
+    			System.out.println("Server said: " + in.nextLine());
+    		}
+    		
+    	} catch (IOException e) {
+    		System.out.println("Error in client: " + e.getMessage());
+    	}
     }
 }
--- fixme-parent/fixme-router/src/main/java/za/co/wethinkcode/App.java
+++ fixme-parent/fixme-router/src/main/java/za/co/wethinkcode/App.java
@@ -1,20 +1,68 @@
 package za.co.wethinkcode;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-
     private static final Logger logger = LogManager.getLogger(App.class.getName());
 
     public static void main( String[] args )
     {
-        logger.info("Hello from log4j 2");
-        System.out.println("Hello from router!");
+    	try (ServerSocket listener = new ServerSocket(5000)) {
+    		logger.info("The capitalization server is running");
+    		ExecutorService pool = Executors.newFixedThreadPool(20);
+    		while (true) {
+    			pool.execute(new Capitalizer(listener.accept()));    			
+    		}
+    		
+    	} catch (IOException e) {
+    		System.out.println(e);
+    	}
     }
+}
+
+class Capitalizer implements Runnable {
+
+	private Socket socket;
+	
+	Capitalizer(Socket s) {
+		this.socket = s;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Connected: " + socket);
+		
+		try {
+			Scanner in = new Scanner(socket.getInputStream());
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			while (in.hasNextLine()) {
+				System.out.println("Client said: " + in.nextLine());
+				String line = in.nextLine();
+				out.print(line);
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				System.out.println("Error closing socket");				
+			}
+			System.out.println("Socket closed");
+		}
+		
+	}
+	
 }
