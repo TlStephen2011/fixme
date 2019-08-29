0e2696f3025363cb6bd8af0a075fdbfc08636254
Tyrone Stephen <tstephen@student.wethinkcode.co.za>
2019-08-29 13:09:50 +0200

    test push in sourcetree

--- fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/App.java
+++ fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/App.java
@@ -7,21 +7,11 @@ import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args ) throws Exception {
-    	try (Socket s = new Socket("127.0.0.1", 5000)) {
-    		System.out.println("Enter lines to capitalize: Ctrl+D to quit");
-    		
-    		Scanner scanner = new Scanner(System.in);
-    		Scanner in = new Scanner(s.getInputStream());
-    		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-    		
-    		while (scanner.hasNextLine()) {
-    			out.println(scanner.nextLine());
-    			System.out.println("Server said: " + in.nextLine());
-    		}
-    		
-    	} catch (IOException e) {
-    		System.out.println("Error in client: " + e.getMessage());
-    	}
+    public static void main( String[] args ) throws Exception {    	
+    
     }
+}
+
+class Transaction {
+	
 }
