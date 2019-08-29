ada780ddd5a9e0b1ed91ce5128546008e09b5f3e
tstephen <tstephen@iqbusiness.net>
2019-08-29 17:48:35 +0200

    It works :D

--- fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/broker/Broker.java
+++ fixme-parent/fixme-broker/src/main/java/za/co/wethinkcode/broker/Broker.java
@@ -8,21 +8,28 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Broker {
 	
 	private String brokerId;
     private final int PORT = 5000;
     private final String HOST = "127.0.0.1";
+    Socket socket;
     
     public Broker() throws UnknownHostException, IOException{
+    	socket = new Socket(HOST, PORT);
+    	System.out.println("Connection to router has been established");    	    	
     }
 
     public void sendMessage(Transaction t) throws IOException {
+    	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    	out.println("Yo watsup");
     }
 
     public void processResponse() throws IOException {
-    	
+    	Scanner in = new Scanner(socket.getInputStream());
+    	System.out.println(in.nextLine());
     }
     
 }
