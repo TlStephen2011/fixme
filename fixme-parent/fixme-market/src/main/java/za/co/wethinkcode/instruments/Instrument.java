5d489220b41c791ed10c68a655ef99a85898a9fd
tstephen <tstephen@iqbusiness.net>
2019-09-02 22:14:33 +0200

    working instrument reader, addedd dependency to guava. Working on encoding and decoding broadcast.

--- fixme-parent/fixme-market/src/main/java/za/co/wethinkcode/instruments/Instrument.java
+++ fixme-parent/fixme-market/src/main/java/za/co/wethinkcode/instruments/Instrument.java
@@ -4,4 +4,14 @@ public class Instrument {
 	public int id;
 	public String instrument;
 	public int reserveQty;
+	
+	public Instrument(int id, String instrument, int reserveQty) {
+		this.id = id;
+		this.instrument = instrument;
+		this.reserveQty = reserveQty;
+	}
+	
+	public Instrument() {
+		
+	}
 }
--- fixme-parent/fixme-market/src/main/java/za/co/wethinkcode/instruments/InstrumentReader.java
+++ fixme-parent/fixme-market/src/main/java/za/co/wethinkcode/instruments/InstrumentReader.java
@@ -1,19 +1,15 @@
 package za.co.wethinkcode.instruments;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Stream;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 
 public class InstrumentReader {
 	
@@ -22,55 +18,26 @@ public class InstrumentReader {
 	
 	public static List<Instrument> getInstruments() throws Exception {
 		
-		String pathToJson = getFilePath();
 		ObjectMapper mapper = new ObjectMapper();
 		
-		String jsonInput = readFile(pathToJson);
-		
+		String jsonInput = null;
+		try {
+			jsonInput = readFile();					
+		} catch (IOException e) {
+			System.out.println("Unable to get jsonInput from file");
+			List<Instrument> toReturn = new ArrayList<Instrument>();
+			toReturn.add(new Instrument(1, "USD", 1500));
+			return toReturn;
+		}
+
 		List<Instrument> instruments = mapper.readValue(jsonInput, new TypeReference<List<Instrument>>(){});
 						
 		return instruments;
-	}
+	}	
 	
-	private static String getFilePath() {
-		StringBuilder b = new StringBuilder();
-		
-		// Get path to current module and separator
-		//String modulePath = new File("").getAbsolutePath();
-		// Path improvement
-	//	String modulePath = Paths.get("").toAbsolutePath().toString();
-	
-		String modulePath = System.getProperty("user.dir");
-		
-		String systemFileSeparator = System.getProperty("file.separator");
-		
-		b.append(modulePath);
-		//b.append(systemFileSeparator);
-		//b.append("fixme-market");
-		b.append(systemFileSeparator);
-		b.append("src");
-		b.append(systemFileSeparator);
-		b.append("main");
-		b.append(systemFileSeparator);
-		b.append("resources");
-		b.append(systemFileSeparator);
-		b.append(readableMarkets[marketChoice] + ".json");
-		
-		return b.toString();		
-	}
-	
-	private static String readFile(String filePath)
-	{
-	    StringBuilder contentBuilder = new StringBuilder();
-	    try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
-	    {
-	        stream.forEach(s -> contentBuilder.append(s).append("\n"));
-	    }
-	    catch (IOException e)
-	    {
-	    	System.out.println("Unable to read file");
-	        e.printStackTrace();
-	    }
-	    return contentBuilder.toString();
+	private static String readFile() throws IOException
+	{				
+		URL url = Resources.getResource(readableMarkets[marketChoice] + ".json");
+		return Resources.toString(url, Charsets.UTF_8);		
 	}
 }
